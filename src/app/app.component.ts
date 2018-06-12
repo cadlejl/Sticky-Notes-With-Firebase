69f88a6f3161ba2f407a3880d44a04f13e0a1169
Jaime Cadle <jaime@coficiency.com>
2018-06-12 12:10:15 -0500

    Build app.component and task model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+import { Task } from "./model/task";
+
 @Component({
+  /*moduleId: module.id, From old version. Don't know if it will be needed */
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app'; // Don't know what this is for.
+
+  private tasks: Task[] = [];
+    private currentTask = new Task(null, false);
+
+    addTask() {
+        let task = new Task(this.currentTask.content, this.currentTask.completed);
+        this.tasks.push(task);
+        this.currentTask.content = null;
+    }
 }
