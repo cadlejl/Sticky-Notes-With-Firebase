d63c04fc02f7a1a597ab24c6058beae1bbef8d78
Jaime Cadle <jaime@coficiency.com>
2018-06-24 11:45:46 -0500

    Install firebase; make decision to convert card.component to note.compoent.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 // Addition
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CardComponent } from './card/card.component';
+
+import { Firebase } from "firebase/database";
+
+// const appRoutes: Routes = [
+//   { 
+//     path: "client/:id", 
+//     component: ClientDetailsComponent
+//   }
+// ]
+
+// Initialize Firebase
+const fbConfig = {
+  apiKey: "AIzaSyBrane3AFXgBbvk-oKKdogxBdcYLTwALqI",
+  authDomain: "sticky-notes-fb.firebaseapp.com",
+  databaseURL: "https://sticky-notes-fb.firebaseio.com",
+  projectId: "sticky-notes-fb",
+  storageBucket: "sticky-notes-fb.appspot.com",
+  messagingSenderId: "248265280533"
+};
+//firebase.initializeApp(config);
 
 @NgModule({
   declarations: [
